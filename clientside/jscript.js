e65bd95e0d61ad5c280612329657e42ff10afb43
shaun deshler <shaundeshler@shauns-MacBook-Pro.local>
2019-11-30 23:44:38 -0800

    cleaning up comments

--- clientside/jscript.js
+++ clientside/jscript.js
@@ -55,24 +55,6 @@ let move = () => {
     dx += 1.3;
     dy += 0.005;
     dz += 0.01;
-    if(i < 100){
-//      particles.material.colors.setHex(0xffffff);
-    }
-/*
-    particles.vertices[i].x *= 1.009;
-    particles.vertices[i].y *= 1.009;
-    particles.vertices[i].z *= 1.009;
-
-   x = particles.vertices[i].x;
-   y = particles.vertices[i].y;
-   z = particles.vertices[i].z;
-
-   particles.vertices[i].x = mapRange(Math.sin(x), -1, 1, -50, 50);
-   particles.vertices[i].y = mapRange(Math.sin(y), -1, 1, -50, 50);
-   particles.vertices[i].z = mapRange(Math.sin(z), -1, 1, -50, 50);
-//   particles.vertices[i].y = mapRange(Math.sin(y), -1, 1, 0, 100);
-*/
-
   }
 }
 console.log(pointCloud)
@@ -80,19 +62,6 @@ var animate = function () {
   requestAnimationFrame( animate );
 
   move()
-//a = particles.vertices[0].x
-//b = particles.vertices[0].y
-
-//console.log(100 * Math.sin(a))
-/*
-b = 5 * Math.sin(a);
-particles.vertices[0].x = b;
-a += 0.2;
-*/
-//particles.vertices[0].x = mapRange(Math.sin(a), -1, 1, -50, 50);
-//particles.vertices[0].y = mapRange(Math.sin(b), -1, 1, -50, 50);
-
-//particles.vertices[0].x = (a - -1) / (1 - -1) * (0.1 - 0) + 0.1;
 
   particles.verticesNeedUpdate = true;
   particles.colorsNeedUpdate = true;
