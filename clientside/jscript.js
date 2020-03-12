7088f9df8a9d296bf54f0682af85e3471ba0dc17
shaun deshler <shaundeshler@shauns-MacBook-Pro.local>
2020-03-11 18:43:37 -0700

    added the lut file to get heatmap gradient

--- clientside/jscript.js
+++ clientside/jscript.js
@@ -9,62 +9,33 @@ document.body.appendChild( renderer.domElement );
 
 camera.position.set(0,0,120);
 
-let particles = new THREE.Geometry();
+let lut = new Lut('cooltowarm')
+lut.setMin(0);
+lut.setMax(1000)
 
-let parMaterial = new THREE.PointCloudMaterial({
-  size: 1,
-  color: 0x000000
+let sphere = new THREE.SphereGeometry(40, 20, 20);
+
+var material = new THREE.MeshBasicMaterial( {
+  color: 0xffffff,
+  vertexColors: THREE.VertexColors
+} );
+
+let mesh = new THREE.Mesh(sphere, material);
+
+scene.add(mesh);
+
+console.log(sphere)
+
+sphere.faces.map((c, i) => {
+  c.color = lut.getColor(i)
 })
 
-let x;
-let y;
-let z;
-
-for(let i = 0; i < 10000; i++){
-  x = (Math.random() * 100) - 50;
-  y = (Math.random() * 100) - 50;
-  z = (Math.random() * 100) - 50;
-
-  particles.vertices.push(new THREE.Vector3(x, y, z))
-}
-
-let pointCloud = new THREE.PointCloud(particles, parMaterial)
-
-scene.add(pointCloud);
-
-let dx = 0;
-let dy = 0;
-let dz = 0;
-
-let move = () => {
-  let constraintX;
-  let constraintY;
-  let constraintZ;
-  let offset = 0;
-  let x;
-
-  for(let i = 0; i < 10000; i++){
-    constraintX = .008 * Math.sin(dx);
-    constraintY = 0.03 * Math.sin(dy);
-    constraintZ = 1 * Math.sin(dz);
-
-    particles.vertices[i].x += constraintX;
-    particles.vertices[i].y += constraintY;
-    particles.vertices[i].z += constraintZ;
-
-    dx += 1.3;
-    dy += 0.005;
-    dz += 0.01;
-  }
-}
-console.log(pointCloud)
 var animate = function () {
   requestAnimationFrame( animate );
 
-  move()
-
-  particles.verticesNeedUpdate = true;
-  particles.colorsNeedUpdate = true;
+  sphere.verticesNeedUpdate = true;
+  sphere.colorsNeedUpdate = true;
+  mesh.colorsNeedUpdate = true;
   renderer.render( scene, camera );
 };
 
--- server.js
+++ server.js
@@ -4,4 +4,4 @@ let app = express();
 
 app.use(express.static('clientside'))
 
-app.listen(5000, console.log('server is up and running'));
+app.listen(5000, console.log('server is up and running on port 5000'));
